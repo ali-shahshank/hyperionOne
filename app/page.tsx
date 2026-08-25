307262b80f8289cafe469def257706e0b40b813e
ali-shahshank <sploitfy@gmail.com>
2026-08-25 12:30:08 +0500

    MINOR -/ Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
 import './globals.css';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Nav from '@/components/Nav';
 
 const page = () => {
   return (
     <>
+      <Nav />
       <Box sx={{ m: 0, p: 4, bgcolor: '#f7f7f7' }}>
         <Typography
-          variant="h2"
+          variant="h3"
           sx={{ textAlign: 'center', fontWeight: '400' }}
         >
-          The Ultimate AI Productivity Platform
+          The Ultimate AI{' '}
+          <Box
+            component="span"
+            sx={{ color: 'blue', fontWeight: '700' }}
+          >
+            Productivity Platform
+          </Box>{' '}
         </Typography>
-        <Typography variant="h3">Subheading Text</Typography>
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: 'center',
+            fontWeight: '300',
+            color: 'text.secondary',
+          }}
+        >
+          Subheading Text
+        </Typography>
       </Box>
     </>
   );
